844a895af7311424e3585acc5360a5ad427a8f82
mdago <dagostino.marco@outlook.com>
2018-10-05 15:12:28 -0400

    Made the reply form show only when logged in

--- templates/topic.php
+++ templates/topic.php
@@ -42,14 +42,18 @@
 	<?php endforeach; ?>
     </ul>
     <h3>Reply To Topic</h3>
-    <form role="form">				
+	<?php if(isLoggedIn()) : ?>
+    <form role="form" method="post" action="topic.php?id=<?php echo $topic->id;?>">				
         <div class="form-group">
-            <textarea id="reply" rows="10" cols="80" class="form-control" name="reply"></textarea>
+            <textarea id="reply" rows="10" cols="80" class="form-control" name="body"></textarea>
             <script>
                 CKEDITOR.replace( 'reply' );
             </script>
         </div>
-            <button type="submit" class="btn btn-default">Submit</button>
+            <button name="do_reply" type="submit" class="btn btn-default">Submit</button>
     </form>
+	<?php else : ?>
+		<p>Please log in to reply</p>
+	<?php endif;?>
                 
 <?php include('includes/footer.php');?>					

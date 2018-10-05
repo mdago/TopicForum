99aa53106f93d33ed0a7d0ca1c0c9ee363f096c9
mdago <dagostino.marco@outlook.com>
2018-10-05 15:15:11 -0400

    create validation and reply validation

--- create.php
+++ create.php
@@ -5,6 +5,38 @@ require('core/ini.php');
 
 
 <?php
+//Create topic object
+$topic = new Topic;
+
+if(isset($_POST['do_create'])){
+    
+    //Create Validator object
+    $validate = new Validator;
+
+    //Create Data Array to contain create form information
+    $data = array();
+    $data['title'] = $_POST['title'];
+    $data['body'] = $_POST['body'];
+    $data['category_id'] = $_POST['category'];
+    $data['user_id'] = getUser()['user_id'];
+    $data['last_activity'] = date('Y-m-d H:i:s');
+
+    //Required Fields
+    $field_array = array('title', 'body', 'category');
+
+    if($validate->isRequired($field_array)){
+		//Register User
+		if($topic->create($data)){
+			redirect('index.php', 'Your topic has been posted', 'success');
+		} else {
+			redirect('topic.php?id='.$topic_id, 'Something went wrong with your post', 'error');
+		}
+	} else {
+		redirect('create.php', 'Please fill in all required fields', 'error');
+	}
+
+}
+
 //Get Template and assign vars
 $template = new Template('templates/create.php');
 
--- topic.php
+++ topic.php
@@ -11,6 +11,33 @@ $topic = new Topic;
 
 //Get Id From url
 $topicId = $_GET['id'];
+
+//Process Reply
+if(isset($_POST['do_reply'])){
+	//Create Data Array
+	$data = array();
+	$data['topic_id'] = $_GET['id'];
+	$data['body'] = $_POST['body'];
+	$data['user_id'] = getUser()['user_id'];
+
+	//Create Validator Object
+	$validate = new Validator;
+	
+	//Required Fields
+	$field_array = array('body');
+	
+	if($validate->isRequired($field_array)){
+		//Register User
+		if($topic->reply($data)){
+			redirect('topic.php?id='.$topic_id, 'Your reply has been posted', 'success');
+		} else {
+			redirect('topic.php?id='.$topic_id, 'Something went wrong with your reply', 'error');
+		}
+	} else {
+		redirect('topic.php?id='.$topic_id, 'Your reply form is blank!', 'error');
+	}
+}
+
 //Get Template and assign vars
 $template = new Template('templates/topic.php');
 
